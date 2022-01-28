9de14ebba8cb2f4d62ad71cadb93879d0aec9a72
christian chacon <christian_kazuma@hotmail.com>
2022-01-28 17:38:32 -0500

    Se crean los ocmponentes home y departamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { DepartamentoComponent } from './departamento/departamento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    DepartamentoComponent
   ],
   imports: [
     BrowserModule
